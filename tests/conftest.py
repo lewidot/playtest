6d73bdd7729eb4e30c48ce6a95db351f80b444f4
Lewis Evans <lewisevansuk@gmail.com>
2023-10-11 17:41:10 +0100

    add reporting config for html report and playwright artifacts

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,30 @@
+from datetime import datetime
+import os
 import pytest
+from pytest import Config
 from playwright.sync_api import Page
 from pages.login_page import LoginPage
 from pages.products_page import ProductsPage
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config: Config):
+    # create reporting directory if not already existing
+    if not os.path.exists("reporting"):
+        os.makedirs("reporting")
+
+    # create a directory with the current date
+    reporting_dir = f"reporting/{datetime.now().strftime('%d-%m-%Y')}"
+
+    if not os.path.exists(reporting_dir):
+        os.makedirs(reporting_dir)
+
+    # create a subdirectory for each execution per day
+    execution_dir = f"{reporting_dir}/{datetime.now().strftime('%d-%m-%Y_%H%M%S')}/"
+
+    # set the command line options for the html and playwright output paths
+    config.option.htmlpath = execution_dir + "/report.html"
+    config.option.output = execution_dir + "artifacts"
 
 
 @pytest.fixture(scope="session")
