e3bb5fc5122c9ed6e88263776a1d7141e759b89a
Lewis Evans <lewisevansuk@gmail.com>
2023-10-11 15:12:50 +0100

    initial refactor with page object model

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,22 +1,29 @@
 from playwright.sync_api import Page, expect
 
+from pages.login_page import LoginPage
+
 
 def test_title(page: Page) -> None:
     """Demo test case."""
-    page.goto("https://www.saucedemo.com")
-    expect(page).to_have_title("Swag Labs")
+    login_page = LoginPage(base_url="https://www.saucedemo.com", page=page)
+
+    # Go to the login page
+    login_page.load()
+
+    # Assert the page title
+    expect(login_page.page).to_have_title(login_page.title)
 
 
 def test_login(page: Page) -> None:
     """Test the login page."""
 
+    login_page = LoginPage(base_url="https://www.saucedemo.com", page=page)
+
     # Go to the login page
-    page.goto("https://www.saucedemo.com")
+    login_page.load()
 
     # Complete the login page
-    page.get_by_placeholder("Username").fill("standard_user")
-    page.get_by_placeholder("Password").fill("secret_sauce")
-    page.get_by_role("button", name="Login").click()
+    login_page.login()
 
     # Assert that the login is successful
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
