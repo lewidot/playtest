ca28afa78015e64250de4c858b7d7a6f3584cd22
Lewis Evans <lewisevansuk@gmail.com>
2023-10-11 15:43:48 +0100

    add base url fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,19 @@ from pages.login_page import LoginPage
 from pages.products_page import ProductsPage
 
 
+@pytest.fixture(scope="session")
+def base_url() -> str:
+    """Fixture to share the base url string."""
+    return "https://www.saucedemo.com"
+
+
 @pytest.fixture
-def login_page(page: Page) -> LoginPage:
+def login_page(page: Page, base_url: str) -> LoginPage:
     """Initialise a LoginPage instance"""
-    return LoginPage(base_url="https://www.saucedemo.com", page=page)
+    return LoginPage(base_url=base_url, page=page)
 
 
 @pytest.fixture
-def products_page(page: Page) -> ProductsPage:
+def products_page(page: Page, base_url: str) -> ProductsPage:
     """Initialise a ProductsPage instance"""
-    return ProductsPage(base_url="https://www.saucedemo.com", page=page)
+    return ProductsPage(base_url=base_url, page=page)
