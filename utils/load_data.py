3af852385380b89624c53a7424e6ec7b51cadb52
Lewis Evans <lewisevansuk@gmail.com>
2023-10-13 16:59:23 +0100

    add load_json_data function

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,6 +1,7 @@
 """File containing functions for loading test data"""
 
 from csv import reader
+import json
 
 
 def load_csv_data(path: str) -> list[tuple]:
@@ -14,3 +15,13 @@ def load_csv_data(path: str) -> list[tuple]:
 
         # transform csv rows into a list of tuples
         return list(map(tuple, csv_reader))
+
+
+def load_json_data(path: str) -> list[tuple]:
+    """Load json data for test parametrization"""
+    with open(path, "r") as f:
+        # load the json data
+        data = json.load(f)
+
+        # transform json values into a list of tuples
+        return [tuple(d.values()) for d in data]
