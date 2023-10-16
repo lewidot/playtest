cbb387d1864fef7061e3b0d51fc0c3007a664a06
Lewis Evans <lewisevansuk@gmail.com>
2023-10-16 17:05:35 +0100

    add load_excel_data function

--- utils/load_data.py
+++ utils/load_data.py
@@ -2,6 +2,7 @@
 
 from csv import reader
 import json
+from openpyxl import load_workbook
 
 
 def load_csv_data(path: str) -> list[tuple]:
@@ -25,3 +26,39 @@ def load_json_data(path: str) -> list[tuple]:
 
         # transform json values into a list of tuples
         return [tuple(d.values()) for d in data]
+
+
+def load_excel_data(path: str) -> list[tuple]:
+    """Load excel data for test parametrization.
+
+    - Workbook must have single active sheet
+
+    - Must have a header row with column headers
+
+    - Header row columns must not be empty
+    """
+
+    # load workbook
+    wb = load_workbook(path, read_only=True)
+
+    # get the first worksheet
+    sheet = wb.worksheets[0]
+
+    # get the first row of the sheet (header row)
+    header_row = next(sheet.rows)
+
+    # get the index of the first header column without a value
+    for idx, header in enumerate(header_row):
+        if header.value == "":
+            max_col_index = idx
+            break
+        else:
+            max_col_index = sheet.max_column
+
+    # create an iterator of rows, skipping header row (1 indexed)
+    rows = sheet.iter_rows(
+        min_row=2, max_row=sheet.max_row, max_col=max_col_index, values_only=True
+    )
+
+    # return a list of tuples with data from each row
+    return [row for row in rows if row[0] is not None]
