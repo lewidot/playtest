d731a228d0aa9ea77ef40ef59cde027dd9da6bd6
Lewis Evans <lewisevansuk@gmail.com>
2023-11-16 16:57:11 +0000

    add functionality for adding product to cart

--- pages/products_page.py
+++ pages/products_page.py
@@ -19,11 +19,22 @@ class ProductsPage:
         self.product_price: Locator = self.page.locator(".inventory_item_price")
         self.product_description: Locator = self.page.locator(".inventory_item_desc")
         self.product_image: Locator = self.page.locator("img")
+        self.product_add_to_cart: Locator = self.page.get_by_text("Add to cart")
+        self.shopping_cart_link: Locator = self.page.locator(".shopping_cart_link")
 
     def load(self: "ProductsPage") -> None:
         """Load the website url."""
         self.page.goto(url=self.url)
 
+    def click_shopping_cart(self: "ProductsPage") -> None:
+        """Click the shopping cart link."""
+        self.shopping_cart_link.click()
+
     def product_by_name(self: "ProductsPage", name: str) -> Locator:
         """Filter a product Locator by the product_name Locator."""
         return self.product.filter(has=self.product_name.filter(has_text=name))
+
+    def add_product_to_cart(self: "ProductsPage", name: str) -> None:
+        """Locate a product and add it to the cart."""
+        product = self.product_by_name(name)
+        product.locator(self.product_add_to_cart).click()
