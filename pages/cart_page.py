5b591125b33ea2ea1e7bb1bdc36048cefdb6d0d9
Lewis Evans <lewisevansuk@gmail.com>
2023-11-20 16:14:12 +0000

    add checkout button to cart page

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -15,7 +15,12 @@ class CartPage:
         # Locators
         self.cart_item: Locator = self.page.locator(".cart_item")
         self.cart_item_name: Locator = self.page.locator(".inventory_item_name")
+        self.checkout_btn: Locator = self.page.get_by_text("Checkout")
 
     def cart_item_by_name(self: "CartPage", name: str) -> Locator:
         """Filter a cart item Locator by the cart_item_name Locator."""
         return self.cart_item.filter(has=self.cart_item_name.filter(has_text=name))
+
+    def click_checkout_button(self: "CartPage") -> None:
+        """Click the Checkout button."""
+        self.checkout_btn.click()
