855d94dea34b5e08a4435df1429ec7aa3921bed7
Lewis Evans <lewisevansuk@gmail.com>
2025-01-20 19:18:43 +0000

    add /run route that streams playwright output

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { source } from 'sveltekit-sse';
+	let messages = $state(['']);
+	let grep = $state('');
+
+	function openStream() {
+		messages = [''];
+		let btn = document.getElementById('runBtn') as HTMLButtonElement;
+		btn.disabled = true;
+		// POST to "/run" and listen for "message" events
+		const connection = source('/run', {
+			cache: false,
+			open() {
+				console.log('opening connection to /run');
+			},
+			close() {
+				console.log('closing connection to /run');
+			},
+			options: {
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ grep: grep })
+			}
+		});
+
+		const stream = connection.select('message');
+
+		// Subscribe to messages
+		stream.subscribe((message: string) => {
+			// Close the connection when the "end" message has been received.
+			if (message === 'end') {
+				connection.close();
+				btn.disabled = false;
+				return;
+			}
+
+			// Update the array of messages.
+			messages.push(message);
+		});
+	}
+</script>
+
+<h1>Playtest</h1>
+
+<label for="grep">Filter<input bind:value={grep} id="grep" name="grep" type="text" /></label>
+<button id="runBtn" onclick={openStream}>Run</button>
+
+<!-- Display messages -->
+<div class="message-container">
+	{#each messages as message}
+		<pre>{message}</pre>
+	{/each}
+</div>
+
+<style>
+	.message-container {
+		border-width: 1px;
+		border-color: black;
+		border-style: solid;
+		border-radius: 6px;
+		min-height: 60px;
+		padding: 1em;
+		margin-top: 2em;
+		min-width: 50%;
+		max-width: 75%;
+	}
+</style>
---
+++ src/routes/run/+server.ts
@@ -0,0 +1,53 @@
+import { produce } from 'sveltekit-sse';
+import { spawn } from 'child_process';
+import stripAnsi from 'strip-ansi';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ request }) => {
+	// Handle the POST request here
+	const jsonData: { grep: string } = await request.json();
+
+	// Return the produce function from 'sveltekit-sse' that handles streaming the response.
+	return produce(async function start({ emit }) {
+		// Base arguments for the playwright cli.
+		const args = ['exec', 'playwright', 'test'];
+
+		// If a grep value is specified, add to playwright cli args.
+		if (jsonData.grep !== '') {
+			args.push('-g', jsonData.grep);
+		}
+		// Spawn the process that runs the playwright tests.
+		const child = spawn('pnpm', args, {
+			cwd: './tmp'
+		});
+
+		// As each data event is emitted, clean up the data and then stream a message.
+		child.stdout.on('data', (data) => {
+			const stringData = data.toString();
+			const stripped = stripAnsi(stringData);
+			const { error } = emit('message', stripped);
+			if (error) {
+				console.error('error whilst streaming');
+				return;
+			}
+		});
+
+		child.stderr.on('data', (data) => {
+			console.error(`stderr: ${data}`);
+		});
+
+		child.on('error', (err) => {
+			console.error(`stderr: ${err}`);
+		});
+
+		// When the process closes, handle any cleanup and send a final message to the client to signal it has completed.
+		child.on('close', (code) => {
+			console.log(`child process exited with code ${code}`);
+			const { error } = emit('message', 'end');
+			if (error) {
+				console.log('error whilst streaming');
+				return;
+			}
+		});
+	});
+};
