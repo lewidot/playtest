423a3030fdb284618968eec9140cf5380935d82d
Lewis Evans <lewisevansuk@gmail.com>
2025-03-28 21:19:30 +0000

    add init function on server start

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,17 @@
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, ServerInit } from '@sveltejs/kit';
 import log from '$lib/logger';
+import * as fs from 'fs';
+
+export const init: ServerInit = async () => {
+	// Check if a playwright project is in the './tmp' directory.
+	const tmpDirPath = './tmp';
+	const tmpDir = fs.readdirSync(tmpDirPath);
+
+	if (tmpDir.length === 0) {
+		console.error('error: playwright project required in the tmp/ directory');
+		process.exit(1);
+	}
+};
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
